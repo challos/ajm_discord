c8ccdf0ac7340a9cff1b0b40127851becb540ca1
Andrew McGrail <andrewjerrismcgrail@gmail.com>
2022-02-05 14:01:47 -0500

    Divided up cog into multiple cogs.

--- amcgrail_bot.py
+++ amcgrail_bot.py
@@ -3,7 +3,22 @@ from discord.ext import commands
 from discord.commands import Option
 
 
-class Base_Bot(commands.Cog):
+class Base_Cog(commands.cog):
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
+
+    async def log_resp(
+        self,
+        ctx: discord.ApplicationContext,
+        string: str,
+        **kwargs,
+    ) -> discord.Interaction:
+        """For debugging. Just prints whatever string content there was before passing it off to ctx.respond."""
+        print(string)
+        return await ctx.respond(string, **kwargs)
+
+
+class Listener_Cog(Base_Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -14,35 +29,34 @@ class Base_Bot(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: discord.ApplicationContext, error):
-        self.send_and_print_str(ctx, "THERE WAS AN ERROR: {}".format(error))
-
-    async def send_and_print_str(
-        self,
-        ctx: discord.ApplicationContext,
-        string: str,
-        **kwargs,
-    ) -> discord.Interaction:
-        """For debugging. Just prints whatever string content there was before passing it off to ctx.respond."""
-        print(string)
-        return await ctx.respond(string, **kwargs)
+        self.log_resp(ctx, "THERE WAS AN ERROR: {}".format(error))
 
     def to_be_deleted(self, msg: discord.Message) -> bool:
         """Just a helper function to decide if a message was sent by this bot."""
         # delete anything done by the bot
         return msg.author.id == self.bot.user.id
 
+
+class Delete_Cog(Base_Cog):
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
+
     @commands.slash_command(
         name="purge_thread",
         description="Deletes all messages from this bot in a thread.",
     )
     async def purge_thread(self, ctx: discord.ApplicationContext):
-        # input args aren't used, but they're here for the future
         channel = self.bot.get_channel(ctx.channel_id)
         if (
             channel.type != discord.ChannelType.public_thread
             and channel.type != discord.ChannelType.private_thread
         ):
-            await ctx.respond("Sorry, this can only be done in a private thread.")
+            await self.log_resp(
+                self,
+                "Sorry {}, this can only be done in a private thread.".format(
+                    ctx.author.display_name
+                ),
+            )
             return
 
         await ctx.channel.purge(limit=1000, check=self.to_be_deleted)
@@ -59,4 +73,13 @@ class Base_Bot(commands.Cog):
             print("Deleted message by {}'s request.".format(ctx.author.display_name))
 
         else:
-            await ctx.respond("Sorry, this message can't be deleted.")
+            await self.log_resp(
+                ctx,
+                "Sorry {}, this message can't be deleted.".format(
+                    ctx.author.display_name
+                ),
+            )
+
+class Amcgrail_Cog(Listener_Cog, Delete_Cog):
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
